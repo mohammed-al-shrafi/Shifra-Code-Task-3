bf03d369d573ebc544200e09c7301f3bf9e659ad
mohammed-al-shrafi <m14.alshrafi7@gmail.com>
2025-12-08 22:48:03 +0200

    Task 3

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const ul = document.querySelector('.nav-bar__links');
+const linksTags = document.querySelectorAll('.nav-bar__links li a');
+const hamburger = document.querySelector('.hamburger');
+
+// hamburger.addEventListener('click', () => {
+//   linksTags.forEach((link) => {
+//     link.addEventListener('click', (e) => {
+//       // remove active from all
+//       linksTags.forEach((l) => l.classList.remove('active'));
+//       // add to clicked
+//       e.currentTarget.classList.add('active');
+//     });
+//   });
+// });
